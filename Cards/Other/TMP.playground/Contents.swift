6c7cd0ccc8adf65e86c9866001de2c08ae08615f
Sergei Smirnov <lam97@yandex.ru>
2022-09-14 21:16:56 +0300

    create a circle

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -3,6 +3,16 @@
 import UIKit
 import PlaygroundSupport
 
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        
+        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+    }
+}
+
 protocol ShapeLayerProtocol: CAShapeLayer {
     init(size: CGSize, fillColor: CGColor)
 }
@@ -13,6 +23,25 @@ extension ShapeLayerProtocol {
     }
 }
 
+class CircleShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let radius = ([size.width, size.height].min() ?? 0) / 2
+        
+        let centerX = size.width / 2
+        let centerY = size.height / 2
+        
+        let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY), radius: radius, startAngle: 0, endAngle: .pi*2, clockwise: true)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 // Present the view controller in the Live View window
-//PlaygroundPage.current.liveView = MyViewController()
+PlaygroundPage.current.liveView = MyViewController()
