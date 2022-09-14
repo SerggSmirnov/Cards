28cc2c04330f816e7c4ad191d03521abf2528f09
Sergei Smirnov <lam97@yandex.ru>
2022-09-14 21:56:51 +0300

    add square

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -9,7 +9,7 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
         self.view = view
         
-        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+        view.layer.addSublayer(SquareShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
     }
 }
 
@@ -33,6 +33,24 @@ class CircleShape: CAShapeLayer, ShapeLayerProtocol {
         let centerY = size.height / 2
         
         let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY), radius: radius, startAngle: 0, endAngle: .pi*2, clockwise: true)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SquareShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let edgeSize = ([size.width, size.height].min() ?? 0)
+        
+        let rect = CGRect(x: 0, y: 0, width: edgeSize, height: edgeSize)
+        let path = UIBezierPath(rect: rect)
         path.close()
         self.path = path.cgPath
         self.fillColor = fillColor
