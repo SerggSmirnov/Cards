dbc0278fff0437da052dd47c55e6aa10bfd79397
Sergei Smirnov <lam97@yandex.ru>
2022-09-15 19:19:33 +0300

    add back side lines

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -9,7 +9,7 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
         self.view = view
         
-        view.layer.addSublayer(BackSideCircle(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+        view.layer.addSublayer(BackSideLine(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
     }
 }
 
@@ -114,6 +114,35 @@ class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
         self.path = path.cgPath
         self.fillColor = fillColor
         self.lineWidth = 1
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        for _ in 1...15 {
+            let randomXStart = Int.random(in: 0...Int(size.width))
+            let randomYStart = Int.random(in: 0...Int(size.height))
+            
+            let randomXEnd = Int.random(in: 0...Int(size.width))
+            let randomYEnd = Int.random(in: 0...Int(size.height))
+            
+            path.move(to: CGPoint(x: randomXStart, y: randomYStart))
+            
+            path.addLine(to: CGPoint(x: randomXEnd, y: randomYEnd))
+        }
+        
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.lineWidth = 3
+        self.lineCap = .round
     }
     
     required init?(coder: NSCoder) {
