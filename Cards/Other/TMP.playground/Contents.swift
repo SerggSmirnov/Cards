c00a2bab49fff106e80d8def55e871059df36c53
Sergei Smirnov <lam97@yandex.ru>
2022-09-15 19:06:26 +0300

    add back side circles

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -9,7 +9,7 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
         self.view = view
         
-        view.layer.addSublayer(CrossShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+        view.layer.addSublayer(BackSideCircle(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
     }
 }
 
@@ -87,6 +87,33 @@ class FillShape: CAShapeLayer, ShapeLayerProtocol {
         let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         self.path = path.cgPath
         self.fillColor = fillColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        for _ in 1...15 {
+            let randomX = Int.random(in: 0...Int(size.width))
+            let randomY = Int.random(in: 0...Int(size.height))
+            let center = CGPoint(x: randomX, y: randomY)
+            path.move(to: center)
+            
+            let radius = Int.random(in: 5...15)
+            
+            path.addArc(withCenter: center, radius: CGFloat(radius), startAngle: 0, endAngle: .pi*2, clockwise: true)
+        }
+        
+        self.path = path.cgPath
+        self.fillColor = fillColor
+        self.lineWidth = 1
     }
     
     required init?(coder: NSCoder) {
