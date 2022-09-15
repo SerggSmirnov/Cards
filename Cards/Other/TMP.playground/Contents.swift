b22819dd87c914eca8af05a6bb29b69da05e0b72
Sergei Smirnov <lam97@yandex.ru>
2022-09-15 18:52:19 +0300

    add fill shape

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -81,5 +81,17 @@ class CrossShape: CAShapeLayer, ShapeLayerProtocol {
     }
 }
 
+class FillShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
