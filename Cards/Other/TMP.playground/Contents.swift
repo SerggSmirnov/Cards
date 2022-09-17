875e46ede480d7ed9c27233d5d2756bf83c103a2
Sergei Smirnov <lam97@yandex.ru>
2022-09-17 09:27:56 +0300

    add properties to card class

--- Cards/Other/TMP.playground/Contents.swift
+++ Cards/Other/TMP.playground/Contents.swift
@@ -153,13 +153,48 @@ class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
 class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
     var isFlipped: Bool = false
     
-    var flipCompletationHandler: ((FlippableView) -> Void)?
+    var flipCompletionHandler: ((FlippableView) -> Void)?
     
     func flip() {
         
     }
     
     var color: UIColor!
+    
+    private let margin: Int = 10
+    
+    lazy var frontSideView: UIView = self.getFrontSideView()
+    
+    lazy var backSideView: UIView = self.getBackSideView()
+    
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(frame: CGRect(x: margin, y: margin, width: Int(self.bounds.width)-margin*2, height: Int(self.bounds.height)-margin*2))
+        
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        return view
+    }
+    
+    private func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        switch ["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLine(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        return view
+    }
     
     init(frame: CGRect, color: UIColor) {
         super.init(frame: frame)
@@ -173,7 +208,7 @@ class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
 
 protocol FlippableView: UIView {
     var isFlipped: Bool { get set }
-    var flipCompletationHandler: ((FlippableView) -> Void)? { get set }
+    var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
     func flip()
 }
 
