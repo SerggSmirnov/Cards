3ba636bff85edffcf8ea10386c66892692ad15b8
Sergei Smirnov <lam97@yandex.ru>
2022-09-23 09:37:15 +0300

    add mvc v1

--- Cards/View/Cards.swift
+++ Cards/View/Cards.swift
@@ -5,4 +5,144 @@
 //  Created by Сергей Смирнов on 22.09.2022.
 //
 
-import Foundation
+import UIKit
+
+class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
+    
+//    var anchorPoint: CGPoint = CGPoint(x: 0, y: 0)
+    
+    var startTouchPoint: CGPoint!
+    
+    var isFlipped: Bool = false {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    var flipCompletionHandler: ((FlippableView) -> Void)?
+    
+    var cornerRadius = 20
+    
+    func flip() {
+        let fromView = isFlipped ? frontSideView : backSideView
+        let toView = isFlipped ? backSideView : frontSideView
+        UIView.transition(from: fromView, to: toView, duration: 0.5, options:
+                            [.transitionFlipFromTop], completion: { _ in self.flipCompletionHandler?(self)})
+        isFlipped = !isFlipped
+    }
+    
+    var color: UIColor!
+    
+    private let margin: Int = 10
+    
+    lazy var frontSideView: UIView = self.getFrontSideView()
+    
+    lazy var backSideView: UIView = self.getBackSideView()
+    
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(frame: CGRect(x: margin, y: margin, width: Int(self.bounds.width)-margin*2, height: Int(self.bounds.height)-margin*2))
+        view.addSubview(shapeView)
+        
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = CGFloat(cornerRadius)
+        
+        return view
+    }
+    
+    private func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        switch ["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLine(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = CGFloat(cornerRadius)
+        
+        return view
+    }
+    
+    private func setupBorders() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    override func draw(_ rect: CGRect) {
+        backSideView.removeFromSuperview()
+        frontSideView.removeFromSuperview()
+        
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startTouchPoint = frame.origin
+        
+        anchorPoint.x = touches.first!.location(in: window).x - frame.minX
+        anchorPoint.y = touches.first!.location(in: window).y - frame.minY
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.frame.origin.x = touches.first!.location(in: window).x - anchorPoint.x
+        self.frame.origin.y = touches.first!.location(in: window).y - anchorPoint.y
+    }
+   /*
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.5) {
+            self.frame.origin = self.startTouchPoint
+            
+            if self.transform.isIdentity {
+                self.transform = CGAffineTransform(rotationAngle: .pi)
+                print("is identity")
+            } else {
+                self.transform = .identity
+                print("else")
+            }
+        }
+    }
+    */
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.frame.origin == startTouchPoint {
+            flip()
+        }
+    }
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        
+        setupBorders()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+protocol FlippableView: UIView {
+    var isFlipped: Bool { get set }
+    var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
+    func flip()
+}
