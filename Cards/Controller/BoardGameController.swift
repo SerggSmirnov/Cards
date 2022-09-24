a9f1e957620229452bf2673b2bb55704571f49f5
Sergei Smirnov <lam97@yandex.ru>
2022-09-24 18:17:32 +0300

    add start button

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -12,18 +12,51 @@ class BoardGameController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var cardsPairsCounts = 8
+    
+    lazy var game: Game = getNewGame()
+    
+    private func getNewGame() -> Game {
+        let game = Game()
+        game.cardCount = self.cardsPairsCounts
+        game.generateCards()
+        return game
     }
-    */
-
+    
+// MARK: UI
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(startButtonView)
+    }
+    
+    lazy var startButtonView = getStartButton()
+    
+    private func getStartButton() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.center.x = view.center.x
+        let scenes = UIApplication.shared.connectedScenes
+//        let window = UIApplication.shared.windows[0]
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        
+        
+        let topPadding = window!.safeAreaInsets.top
+        button.frame.origin.y = topPadding
+        
+        button.setTitle("Start Game", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.gray, for: .highlighted)
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        
+        return button
+    }
+    
+    
 }
+
+
